938bdda97e72fd4f4cfa678d11dc130109eca3df
devblok <l.praninskas@gmail.com>
2018-09-30 15:52:45 +0300

    Inject links

--- fabfile.py
+++ fabfile.py
@@ -1,5 +1,6 @@
 from fabric import task
 from shutil import rmtree
+from shutil import copyfile
 from os import listdir
 from os import mkdir
 from os.path import isfile, join
@@ -9,12 +10,19 @@ from jinja2 import FileSystemLoader
 
 file_prefix = "T--Vilnius-Lithuania--"
 categories_path = "./categories"
+files_path = "files/"
+sass_path = "files/css/"
 files_base_url = "http://2018.igem.org/wiki/images/7/74/T--Vilnius-Lithuania--Cover.jpg"
 pages_base_url = "http://2018.igem.org/Team:Vilnius-Lithuania"
+build_dir = "build/"
+
+file_index = {} # Contains all proper links for files
+
+
+############ Fabric tasks
 
 @task
 def render(ctx):
-    build_dir = "build"
     template_files = [f for f in listdir(categories_path) if isfile(join(categories_path, f))]
 
     try:
@@ -23,33 +31,56 @@ def render(ctx):
         rmtree(build_dir)
         mkdir(build_dir)
 
-    for i, filename in enumerate(template_files):
-        template = load_template(filename)
-        with open(build_dir + '/' + filename.rpartition('.')[0], "wb") as f:
-            page = template.render()
-            f.write(page.encode("utf-8"))
-
-    print(ctx.host)
+    copy_files() # Modifies file_index
+    render_all(template_files) # Uses file_index
 
 
 # This deployment method is for 
-@task
-def deployAPI(ctx):
-    template_files = [f for f in listdir(categories_path) if isfile(join(categories_path, f))]
-    print(template_files)
+#@task
+#def deployAPI(ctx):
+#    template_files = [f for f in listdir(categories_path) if isfile(join(categories_path, f))]
+#    return
 
-    template = load_template("Home.html.j2")
 
-    print(template.render(baseurl=pages_base_url, files_baseurl=files_base_url))
-    return
+############ Render and build tools
+
+
+# Uploads to igem org and collects links
+def upload_files():
+    file_index = {}
+
+
+def copy_files():
+    mkdir(build_dir + files_path)
+    asset_files = [f for f in listdir(files_path) if isfile(join(files_path, f))]
+    for i, asset in enumerate(asset_files):
+        file_index[asset] = files_path + asset
+        copyfile(files_path + asset, build_dir + files_path + asset)
+
+
+def render_all(templates):
+    for i, filename in enumerate(templates):
+        template = load_template(filename)
+        with open(build_dir + filename.rpartition('.')[0], "wb") as f:
+            page = template.render(asset=make_link)
+            f.write(page.encode("utf-8"))
+
 
 def load_template(template):
     loader = FileSystemLoader(["./categories", "./templates"])
     environment = Environment(loader=loader)
     return environment.get_template("Home.html.j2")
 
-# Upload tools
+
+############ igem.org "API"
+
 def edit_page(content):
     # Need to retrieve from edit request:
     # submit form data
     return
+
+
+############ Template function injections
+
+def make_link(asset):
+    return file_index.get(asset)
