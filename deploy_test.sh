54873e0f75f6f89436c018f62fc35480b31b71e1
Kasparas Taminskas <kasparas.taminskas@yahoo.lt>
2018-10-08 22:11:47 +0300

    Major intro page update

--- deploy_test.sh
+++ deploy_test.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 cd "$(dirname "$0")"
-scp -r build/files root@wiki.devblok.me:/usr/share/nginx/html
-scp -r build/Home.html root@wiki.devblok.me:/usr/share/nginx/html
+scp -r build/files root@wiki.devblok.me:/usr/share/nginx/html && scp -r build/Home.html root@wiki.devblok.me:/usr/share/nginx/html && scp -r build/Intro.html root@wiki.devblok.me:/usr/share/nginx/html
 ssh root@wiki.devblok.me
 cd /usr/share/nginx/html
 rm -f index.html
